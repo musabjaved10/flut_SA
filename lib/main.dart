f167706f29ede6409b7c3a41aff9142ba367a2e8
musabjaved10 <musabjaved10@gmail.com>
2021-11-27 23:55:52 +0500

    basic layout

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_details_screen.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.deepOrange)
+      ),
+      home: ProductOverViewScreen(),
+      routes: {
+        ProductDetailsScreen.routeName : (context) => ProductDetailsScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('Lets build the shop App'),
+      ),
+    );
+  }
+}
+
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_details_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem(
+      {Key? key, required this.id, required this.title, required this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () => Navigator.of(context).pushNamed(ProductDetailsScreen.routeName, arguments: id),
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+            backgroundColor: Colors.black54,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary
+
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            )),
+      ),
+    );
+  }
+}
